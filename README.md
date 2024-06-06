862132788dc5cf13aea20047e6a10efb5ef7bb7f
Vikrant <viks120@users.noreply.github.com>
2024-06-06 18:10:43 +0530

    Flexion coding challenge
    Flexion codding challenge

---
+++ README.md
@@ -0,0 +1,46 @@
+# Flexion Coding Challenge
+
+## Installation
+
+1. **Clone the Repository**
+   - Use Git to clone the repository to your local machine:
+     ```bash
+     git clone https://github.com/viks120/FlexionCodeReview
+     ```
+   
+2. **Access the Application**
+   - Navigate to the directory where you cloned the repository.
+   - Open the `index.html` file in your web browser to start using the application.
+   - If you are using vs code IDE, you can use live server extensions.
+
+## Future Development Tasks
+
+Here are the prioritized development tasks to enhance the application:
+
+### 1. UI Interface
+   - **Priority**: High
+   - Develop a more interactive and user-friendly web interface using a framework like React or Angular to improve usability and aesthetics. 
+   - Add bootstap framework to code to support multiple screen sizes.
+
+### 2. Enhance Error Handling and Input Validation
+   - **Priority**: High
+   - Improve client-side validation to ensure that all user inputs are valid before processing. This includes validating unit types and numeric inputs.
+   - Adding pre-populated options based on unit types will assist users in understanding precisely what they need to input.
+
+### 3. Expand Conversion Logic
+   - **Priority**: Medium
+   - Add more unit conversion options such as converting between different length and weight units, thereby increasing the tool's utility.
+
+### 4. Add Unit Tests
+   - **Priority**: Medium
+   - Implement unit testing using Jest to ensure each function reliably performs as expected, which is crucial for maintaining high-quality code.
+
+   
+### 5. Microservices Conversion for API Integration
+   - **Priority**: Low
+   - Convert the application to microservices to enable third-party applications to utilize it.
+   - Create REST enddpoints.
+
+### 6. Data Storage
+   - **Priority**: Low
+   - Add functionality to save previous queries and results with a backend database.
---
+++ script.js
@@ -0,0 +1,146 @@
+function check_answer() {
+
+    let authoritative_answer;
+
+    const temperatures = ["Kelvin", "Celsius", "Fahrenheit", "Rankine"];
+    const voulmes = ["liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"];
+
+    const input_numerical_value = parseFloat(document.getElementById("input_numerical_value").value);
+    const input_unit_of_measure = document.getElementById("input_unit_of_measure").value;
+    const target_unit_of_measure = document.getElementById("target_unit_of_measure").value;
+    const student_response = parseFloat(document.getElementById("student_response").value);
+
+    if(isNaN(student_response)) {
+        return "incorrect";
+    }
+
+    authoritative_answer = get_authorative_answer(temperatures, voulmes, input_numerical_value, 
+        input_unit_of_measure, 
+        target_unit_of_measure, 
+        student_response);
+
+    if (authoritative_answer === "invalid" ) {
+        return "invalid";
+    }
+
+    if (Math.round(authoritative_answer * 10) / 10 === Math.round(student_response * 10) / 10) {
+        return "correct";
+    } else {
+        return "incorrect";
+    }
+}
+
+function update_result() {
+    result = check_answer();
+    document.getElementById("result").innerHTML = '<b>Output:</b> ' + result;
+}
+
+
+function get_authorative_answer(temperatures, 
+                                voulmes, 
+                                input_numerical_value, 
+                                input_unit_of_measure, 
+                                target_unit_of_measure) {
+
+    let response;
+
+    if(temperatures.includes(input_unit_of_measure) && temperatures.includes(target_unit_of_measure)) {
+        response = temperature_conversion(input_numerical_value, input_unit_of_measure, target_unit_of_measure);
+    } else if(voulmes.includes(input_unit_of_measure) && voulmes.includes(target_unit_of_measure)) {
+        response = volume_conversion(input_numerical_value, input_unit_of_measure, target_unit_of_measure);
+    } else {
+        return "invalid";
+    }
+
+    if(response === null) {
+        return "invalid";
+    }
+
+    return response;
+}
+
+function temperature_conversion(input_numerical_value, input_unit_of_measure, target_unit_of_measure) {
+    if(input_unit_of_measure === target_unit_of_measure) {
+        return input_numerical_value;
+    }
+
+    let celsius;
+    switch (input_unit_of_measure) {
+        case "Kelvin":
+            celsius = input_numerical_value - 273.15;
+            break;
+        case "Fahrenheit":
+            celsius = (input_numerical_value - 32) * 5/9;
+            break;
+        case "Rankine":
+            celsius = (input_numerical_value - 491.67) * 5/9;
+            break;
+        case "Celsius":
+            celsius = input_numerical_value;
+            break;
+        default:
+            return null;
+    }
+
+    switch (target_unit_of_measure) {
+        case "Kelvin":
+            return celsius + 273.15;
+        case "Fahrenheit":
+            return celsius * 9/5 + 32;
+        case "Rankine":
+            return (celsius + 273.15) * 9/5;
+        case "Celsius":
+            return celsius;
+        default:
+            return null;
+    }
+}
+
+function volume_conversion(input_numerical_value, input_unit_of_measure, target_unit_of_measure) {
+    const liter_cubic_inch = 0.0163871;
+    const liter_tablespoon = 0.0147868;
+    const liter_cup = 0.236588;
+    const liter_cubic_foot = 28.3168;
+    const liter_gallon = 3.78541;
+
+    let liters;
+    switch (input_unit_of_measure) {
+        case "liters":
+            liters = input_numerical_value;
+            break;
+        case "tablespoons":
+            liters = input_numerical_value * liter_tablespoon;
+            break;
+        case "cubic-inches":
+            liters = input_numerical_value * liter_cubic_inch;
+            break;
+        case "cups":
+            liters = input_numerical_value * liter_cup;
+            break;
+        case "cubic-feet":
+            liters = input_numerical_value * liter_cubic_foot;
+            break;
+        case "gallons":
+            liters = input_numerical_value * liter_gallon;
+            break;
+        default:
+            return null;
+    }
+
+    switch (target_unit_of_measure) {
+        case "liters":
+            return liters;
+        case "tablespoons":
+            return liters / liter_tablespoon;
+        case "cubic-inches":
+            return liters / liter_cubic_inch;
+        case "cups":
+            return liters / liter_cup;
+        case "cubic-feet":
+            return liters / liter_cubic_foot;
+        case "gallons":
+            return liters / liter_gallon;
+        default:
+            return null;
+    }
+}
